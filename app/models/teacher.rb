c4854d1d4428d3a28f5a9b247c5f717f6f6cc2f4
yaro shm <yashmiii@yandex.ru>
2015-04-23 15:30:47 +0200

    -add teacher_id to Lesson -all dependancies between Teacher has many Lessons

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,5 +1,6 @@
 class Teacher < ActiveRecord::Base
   has_many :groups
+  has_many :lessons
   def to_s
     last_name
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150422121720) do
+ActiveRecord::Schema.define(version: 20150423120751) do
 
   create_table "attendances", force: true do |t|
     t.string   "attendance"
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 20150422121720) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "period_id"
+    t.integer  "teacher_id"
   end
 
   create_table "periods", force: true do |t|
