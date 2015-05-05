54abcd659d556fc74d2cc09e85478735da1fbaa5
yaro shm <yashmiii@yandex.ru>
2015-05-05 11:54:15 +0200

    carrierwave LogoUpLoader for company_logo. & start making dependancies between Company and Office

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,2 +1,7 @@
 class Company < ActiveRecord::Base
+  has_many :offices
+  def to_s
+    company_name
+  end
+   mount_uploader :logo, LogoUploader
 end
--- app/models/office.rb
+++ app/models/office.rb
@@ -2,6 +2,7 @@ class Office < ActiveRecord::Base
   has_many :classrooms
   has_many :clients
   has_many :groups
+  belongs_to :company
   def to_s
     name
   end
