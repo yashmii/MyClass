2b4df43e7df3885e681239c7206ed8f239708f96
yaro shm <yashmiii@yandex.ru>
2015-05-09 18:59:30 +0200

    FIX BUGS & REDIRECTS

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -28,7 +28,7 @@ class AttendancesController < ApplicationController
 
     respond_to do |format|
       if @attendance.save
-        format.html { redirect_to @attendance, notice: 'Attendance was successfully created.' }
+        format.html { redirect_to lesson_path(@attendance.lesson), notice: 'Attendance was successfully created.' }
         format.json { render :show, status: :created, location: @attendance }
       else
         format.html { render :new }
@@ -42,7 +42,7 @@ class AttendancesController < ApplicationController
   def update
     respond_to do |format|
       if @attendance.update(attendance_params)
-        format.html { redirect_to @attendance, notice: 'Attendance was successfully updated.' }
+        format.html { redirect_to lesson_path(@attendance.lesson), notice: 'Attendance was successfully updated.' }
         format.json { render :show, status: :ok, location: @attendance }
       else
         format.html { render :edit }
@@ -56,7 +56,7 @@ class AttendancesController < ApplicationController
   def destroy
     @attendance.destroy
     respond_to do |format|
-      format.html { redirect_to attendances_url, notice: 'Attendance was successfully destroyed.' }
+      format.html { redirect_to lesson_path(@attendance.lesson), notice: 'Attendance was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
